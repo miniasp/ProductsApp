04491755ece8eeaff4600354f0b1081317ab600e
Will 保哥 <doggy.huang@gmail.com>
2016-06-17 21:12:13 +0800

    加入 PostProduct() 動作方法 ( 新增資料功能 )

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace ProductsApp.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[] 
+        static List<Product> products = new List<Product>()
         { 
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
@@ -18,17 +19,23 @@ namespace ProductsApp.Controllers
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return products;
+            return ProductsController.products;
         }
 
         public IHttpActionResult GetProduct(int id)
         {
-            var product = products.FirstOrDefault((p) => p.Id == id);
+            var product = ProductsController.products.FirstOrDefault((p) => p.Id == id);
             if (product == null)
             {
                 return NotFound();
             }
             return Ok(product);
+        }
+
+        public IHttpActionResult PostProduct(Product product)
+        {
+            ProductsController.products.Add(product);
+            return Created(Url.Link("DefaultApi", new { id = product.Id }), product);
         }
     }
 }
